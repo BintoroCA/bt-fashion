44560bbf284646acbac8b50321d91abc30bfc093
BintoroCA <bintoro1106@gmail.com>
2022-05-30 12:25:41 +0700

    categories & products

--- src/data.js
+++ src/data.js
@@ -21,3 +21,56 @@ export const sliderItems = [
     bg: "f5fafd",
   },
 ];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://media.istockphoto.com/photos/professional-cook-feeling-satisfied-with-her-new-menu-picture-id1312204470?b=1&k=20&m=1312204470&s=170667a&w=0&h=73FjNBGtZqv3XzmBiPh9abBKvXHfrkJgEsjLTLFcT8k=",
+    title: "CHEF STYLE",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1604904612715-47bf9d9bc670?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dW5pZm9ybXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    title: "FORMAL STYLE",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1621841730147-3cb4dbe0c5c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTR8fGVpZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    title: "RAMADHAN STYLE",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1638232928539-6e91c47ddec5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8dG93ZWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 2,
+    img: "https://media.istockphoto.com/photos/typical-couch-potato-a-man-in-his-underwear-laying-on-the-sofa-and-picture-id1367094334?b=1&k=20&m=1367094334&s=170667a&w=0&h=Iv4iK2hZrhjkZBjzvw8VPhW3waG0jmPadTOqErT7f3w=",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2hvZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1542496658-e33a6d0d50f6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d2F0Y2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 5,
+    img: "https://images.unsplash.com/photo-1624378439575-d8705ad7ae80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGFudHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 6,
+    img: "https://images.unsplash.com/photo-1618354691373-d851c5c3a990?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c2hpcnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 7,
+    img: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdsYXNzZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    id: 8,
+    img: "https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8amFja2V0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+];
