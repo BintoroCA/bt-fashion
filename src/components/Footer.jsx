dbbdbf0476db07e74f3fb008289f732623bf702e
BintoroCA <bintoro1106@gmail.com>
2022-05-30 13:25:52 +0700

    homepage done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,133 @@
+import {
+  Facebook,
+  Instagram,
+  LocationOn,
+  MailOutlined,
+  Phone,
+  Pinterest,
+  Twitter,
+} from "@mui/icons-material";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
+`;
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>BT Fashion</Logo>
+        <Desc>
+          There are many variation of passages of Lorem Ipsum available, but the
+          majority have suffered alteration in some form, by injected humour, or
+          randomised words which don't look even slightly believable.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Whishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <LocationOn style={{ marginRight: "10px" }} /> 777 Nagoya, Batam,
+          Indonesia
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +62 853 8013 3381
+        </ContactItem>
+        <ContactItem>
+          <MailOutlined style={{ marginRight: "10px" }} /> bintoro1106@gmail.com
+        </ContactItem>
+        <Payment src="https://cdn.webshopapp.com/shops/218525/files/371942398/payment-methods-epicerie-ludo.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
